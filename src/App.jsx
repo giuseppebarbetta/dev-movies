bdbb2af4edf1d44b7aa6e53947e986eb89a0b3a7
giuseppebarbetta <giuseppebarbetta@outlook.com>
2024-04-26 22:22:25 -0300

    include api in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Background } from "./components/background";
 import { DescriptionMovie } from "./components/description-movie";
 import { Header } from "./components/header";
@@ -8,18 +9,30 @@ import { Video } from "./components/videos";
 import { apiData } from "./api/data";
 
 function App() {
-  const [video, setVideo] = useState(apiData?.movies[0]);
+  const [video, setVideo] = useState(apiData.movies[0]);
+  const [moviesData, setMoviesData] = useState([])
   const [playVideo, setPlayVideo] = useState(false);
+  const baseUrl = "http://localhost:3003"
 
   const { title, description, linkVideo, imageBanner } = video;
+
+  const apiDataMovies = moviesData?.movies?.length > 0 ? moviesData : apiData
+
+  useEffect(() => {
+    const getMoviesAndCategories = async () => {
+      const { data } = await axios.get(`${baseUrl}/movies-categories`);
+      setMoviesData(data)
+    };
+    getMoviesAndCategories();
+  }, []);
 
   const handleOpenPlayVideo = () => setPlayVideo(true);
   const handleClosePlayVideo = () => setPlayVideo(false);
 
   const handleVideo = (data) => {
-    setVideo(data)
-    window.scrollTo({top: 0})
-  }
+    setVideo(data);
+    window.scrollTo({ top: 0 });
+  };
 
   return (
     <Background imageBanner={imageBanner}>
@@ -31,7 +44,7 @@ function App() {
         description={description}
       />
 
-      <Movies data={apiData} handleVideo={(data) => handleVideo(data)} />
+      <Movies data={apiDataMovies} handleVideo={(data) => handleVideo(data)} />
 
       <Video
         playVideo={playVideo}
